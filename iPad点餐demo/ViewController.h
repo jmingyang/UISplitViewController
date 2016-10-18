0063f240dd4729595da6898d44296acc08bfe151
somesame <863639156@qq.com>
2016-10-18 17:58:16 +0800

    Initial Commit

---
+++ iPad点餐demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPad点餐demo
+//
+//  Created by lucid on 16/10/18.
+//  Copyright © 2016年 Gooou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iPad点餐demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPad点餐demo
+//
+//  Created by lucid on 16/10/18.
+//  Copyright © 2016年 Gooou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
